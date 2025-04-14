bf1d2626ccabafbe655cada585f9e732b64c262f
m4ln <runzmarlen@gmail.com>
2025-04-14 19:27:10 +0800

    updated extract data

--- extract_csv_from_recording.py
+++ extract_csv_from_recording.py
@@ -44,36 +44,58 @@ def load_from_bin(file_path):
 
 if __name__ == "__main__":
 
-    rec_path = "recordings/circle.bin"
+    # number of ppl
+    no_ppl = 7
+    recordings = ['circle', 'line', 'random', 'square', 'triangle', 'cross']
+    rec_path = "recordings"
     
-    decoder = ViveDecoder()
-    
-    data = load_from_bin(rec_path)
-    
-    table = []
-    for d in data:
-        if d is None:
-            continue
-        timestamp = d[0]
-        decoder.decode(d[1])
-        trackers = decoder.vive_trackers
-        if trackers is None or len(trackers) == 0:
-            logging.warning("No trackers found in the decoded data.")
-            continue
-        
-        # augment the data
-        
-        row = [timestamp]
-        for tracker in trackers:
-            if tracker["is_tracked"]:
-                x = tracker["position"][0]
-                y = tracker["position"][2]
-                row.append(x)
-                row.append(y)
-        table.append(row)
-        
-        # write the data to csv
-with open("recordings/circle.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    for row in table:
-        writer.writerow(row)
+    with open("recordings/formations.csv", "w", newline="") as csvfile:
+        # Add a first row with the header 'form_type', 'x{i}, 'y{i}' for each tracker
+        header = ["form_type"]
+        for i in range(0, no_ppl):
+            header.append(f"x{i}")
+            header.append(f"y{i}")
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+        print(f"Header: {header}")
+
+        for recording in recordings:
+            recording_path = os.path.join(rec_path, f"{recording}.bin")
+            if not os.path.exists(recording_path):
+                logging.error(f"Recording file not found: {recording_path}")
+                continue
+
+            decoder = ViveDecoder()
+            
+            data = load_from_bin(recording_path)
+            
+            table = []
+            for d in data:
+                if d is None:
+                    continue
+                # timestamp = d[0]
+                # label is the index of the recording
+                label = recordings.index(recording)
+                print(f"Processing recording: {recording}, label: {label}")
+                decoder.decode(d[1])
+                trackers = decoder.vive_trackers
+                if trackers is None or len(trackers) == 0:
+                    logging.warning("No trackers found in the decoded data.")
+                    continue
+                
+                # augment the data
+                
+                # row = [timestamp]
+                row = [label]
+                for tracker in trackers:
+                    if tracker["is_tracked"]:
+                        x = tracker["position"][0]
+                        y = tracker["position"][2]
+                        row.append(x)
+                        row.append(y)
+                table.append(row)
+                
+                # write the data to csv
+                writer = csv.writer(csvfile)
+                for row in table:
+                    writer.writerow(row)
